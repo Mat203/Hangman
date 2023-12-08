6eec64b64ec16444d02a4f741beb6ae17021ce73
Mat_Tol <matvey.talalaevskiy@gmail.com>
2023-12-08 20:03:39 +0200

    Game classes

--- main.cpp
+++ main.cpp
@@ -1,238 +1,75 @@
 #include <SFML/Graphics.hpp>
+#include <map>
+#include <vector>
+#include <string>
 
-enum class Player
-{
-    None,
-    X,
-    O
+enum Difficulty {
+    Easy,
+    Medium,
+    Hard
 };
 
-class TicTacToe {
-public:
-    TicTacToe() : actionsNumber(0), currentPlayer(Player::X), winner(Player::None)
-    {
-        resetBoard();
-    }
-
-    void reset()
-    {
-        resetBoard();
-        actionsNumber = 0;
-        currentPlayer = Player::X;
-        winner = Player::None;
-    }
-
-    bool isGameOver() const
-    {
-        return winner != Player::None || actionsNumber >= 9;
-    }
-
-    Player getCurrentPlayer() const
-    {
-        return currentPlayer;
-    }
-
-    Player getWinner() const
-    {
-        return winner;
-    }
-
-    void makeMove(int row, int col)
-    {
-        if (!isGameOver() && row >= 0 && row < gridSize && col >= 0 && col < gridSize && board[row][col] == Player::None)
-        {
-            board[row][col] = currentPlayer;
-            checkForWinner(row, col);
-            currentPlayer = (currentPlayer == Player::X) ? Player::O : Player::X;
-            actionsNumber++;
-            return;
-        }
-
-        reset();
-    }
-
-    Player getCell(int row, int col) const
-    {
-        return board[row][col];
-    }
-
+class Hangman {
 private:
-    const int gridSize = 3;
-    int actionsNumber = 0;
-    Player currentPlayer;
-    Player winner;
-    std::vector<std::vector<Player>> board;
-
-    void resetBoard()
-    {
-        board = std::vector<std::vector<Player>>(gridSize, std::vector<Player>(gridSize, Player::None));
+    int hangmanParts;
+public:
+    void drawNext() {
     }
+};
 
-    void checkForWinner(int lastMoveRow, int lastMoveCol)
-    {
-        // Check row
-        if (board[lastMoveRow][0] == board[lastMoveRow][1] && board[lastMoveRow][1] == board[lastMoveRow][2])
-        {
-            winner = board[lastMoveRow][0];
-        }
+class WordManager {
+private:
+    std::map<Difficulty, std::vector<std::string>> wordDictionary;
+public:
+    std::string pickWord(Difficulty difficulty) {
+    }
+    std::string updateGuessedWord(std::string secretWord, std::string guessedWord, char letter) {
+    }
+    void readWordDictionary(std::string filename) {
+    }
+};
 
-        // Check column
-        if (board[0][lastMoveCol] == board[1][lastMoveCol] && board[1][lastMoveCol] == board[2][lastMoveCol])
-        {
-            winner = board[0][lastMoveCol];
-        }
-
-        // Check diagonals
-        if ((lastMoveRow == lastMoveCol || lastMoveRow + lastMoveCol == gridSize - 1) &&
-            ((board[0][0] == board[1][1] && board[1][1] == board[2][2]) ||
-                (board[0][2] == board[1][1] && board[1][1] == board[2][0])))
-        {
-            winner = board[1][1];
-        }
+class HangmanGame {
+private:
+    std::string secretWord;
+    std::string guessedWord;
+    std::vector<char> usedLetters;
+    Difficulty difficulty;
+    Hangman hangman;
+    WordManager wordManager;
+public:
+    HangmanGame(Difficulty difficulty) {
+    }
+    void reset() {
+    }
+    bool isGameOver() {
+    }
+    std::string getGuessedWord() {
+    }
+    std::vector<char> getUsedLetters() {
+    }
+    void guessLetter(char letter) {
     }
 };
 
 class Renderer {
-public:
-    Renderer() : windowSize(300), cellSize(windowSize / 3)
-    {
-        initWindow();
-    }
-
-    void run(TicTacToe& game)
-    {
-        while (window.isOpen())
-        {
-            processEvents(game);
-            render(game);
-        }
-    }
-
 private:
     const int windowSize;
     const int cellSize;
-
     sf::RenderWindow window;
-
-    void initWindow()
-    {
-        window.create(sf::VideoMode(windowSize, windowSize), "Tic-Tac-Toe");
-        window.setFramerateLimit(60);
+public:
+    Renderer() : windowSize(600), cellSize(30) {
     }
-
-    void processEvents(TicTacToe& game)
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-            {
-                window.close();
-            }
-            else if (event.type == sf::Event::MouseButtonPressed)
-            {
-                if (event.mouseButton.button == sf::Mouse::Left)
-                {
-                    handleMouseClick(game, event.mouseButton.x, event.mouseButton.y);
-                }
-            }
-        }
+    void run(HangmanGame game) {
     }
-
-    void handleMouseClick(TicTacToe& game, int mouseX, int mouseY)
-    {
-        int row = mouseY / cellSize;
-        int col = mouseX / cellSize;
-        game.makeMove(row, col);
+    void initWindow() {
     }
-
-    void render(const TicTacToe& game)
-    {
-        window.clear();
-
-        // Draw grid
-        for (int i = 1; i < 3; ++i) {
-            sf::Vertex line[] = {
-                sf::Vertex(sf::Vector2f(i * cellSize, 0)),
-                sf::Vertex(sf::Vector2f(i * cellSize, windowSize)),
-                sf::Vertex(sf::Vector2f(0, i * cellSize)),
-                sf::Vertex(sf::Vector2f(windowSize, i * cellSize))
-            };
-            window.draw(line, 4, sf::Lines);
-        }
-
-        // Draw X and O based on the state of the board
-        for (int row = 0; row < 3; ++row) {
-            for (int col = 0; col < 3; ++col) {
-                if (game.getCell(row, col) == Player::X) {
-                    drawX(row, col);
-                }
-                else if (game.getCell(row, col) == Player::O) {
-                    drawO(row, col);
-                }
-            }
-        }
-
-        // Draw winner message
-        if (game.getWinner() != Player::None || game.isGameOver()) {
-            static sf::Font font;
-            bool bFontLoaded = false;
-            if (!bFontLoaded)
-            {
-                font.loadFromFile("C:\\Users\\User\\Desktop\\IT\\PP\\Hangman\\Arial.ttf"); // Change the font file path as needed
-                bFontLoaded = true;
-            }
-
-            sf::Text text;
-            text.setFont(font);
-            text.setCharacterSize(30);
-            text.setFillColor(sf::Color::White);
-            sf::String Answer = (game.getWinner() == Player::X ? "Player X" : "Player O") + std::string(" wins!\nClick to restart.");
-            if (game.getWinner() == Player::None)
-            {
-                Answer = "Draw!\nClick to restart.";
-            }
-
-            text.setString(Answer);
-            text.setPosition(10, 10);
-            window.clear();
-            window.draw(text);
-        }
-
-        window.display();
+    void processEvents(HangmanGame game) {
     }
-
-    void drawX(int row, int col)
-    {
-        sf::Vertex line1[] = {
-            sf::Vertex(sf::Vector2f(col * cellSize + 10, row * cellSize + 10)),
-            sf::Vertex(sf::Vector2f((col + 1) * cellSize - 10, (row + 1) * cellSize - 10))
-        };
-        sf::Vertex line2[] = {
-            sf::Vertex(sf::Vector2f((col + 1) * cellSize - 10, row * cellSize + 10)),
-            sf::Vertex(sf::Vector2f(col * cellSize + 10, (row + 1) * cellSize - 10))
-        };
-
-        window.draw(line1, 2, sf::Lines);
-        window.draw(line2, 2, sf::Lines);
+    void handleKeyPress(HangmanGame game, char key) {
     }
-
-    void drawO(int row, int col)
-    {
-        sf::CircleShape circle(cellSize / 2 - 10);
-        circle.setPosition(col * cellSize + 10, row * cellSize + 10);
-        circle.setOutlineThickness(10);
-        circle.setOutlineColor(sf::Color::White);
-
-        window.draw(circle);
+    void render(HangmanGame game) {
+    }
+    void drawGameEndMessage(HangmanGame game) {
     }
 };
-
-int main()
-{
-    TicTacToe game;
-    Renderer renderer;
-    renderer.run(game);
-
-    return 0;
-}
